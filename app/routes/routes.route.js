d5c3a75bfd1e1b5b4b9d49d96525bd5bf2d51b62
yuvraj1902 <yuvraj@gkmit.co>
2022-11-11 11:41:47 +0530

    added middlewares and some more changes in routes file

--- app/routes/routes.route.js
+++ app/routes/routes.route.js
@@ -1,8 +1,10 @@
 const {createRoutes,getAllRoutes,deleteRoutes} = require("../controllers/routes.controller");
+const isAdmin=require("../middlewares/isAdmin")
+const isSignedIn=require("../middlewares/isSignedIn")
 const express = require("express");
 const router = express.Router();
 router.post( "/routes", createRoutes);
-router.get("/routes", getAllRoutes);
+router.get("/routes",isSignedIn,isAdmin, getAllRoutes);
 router.delete("/routes",  deleteRoutes);
 
 module.exports = router;
